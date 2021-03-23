f948d9924ac47eadf84e98f7780b39e5f06abfe8
Sofia Acerbi <sofiacerbi00@gmail.com>
2021-03-23 17:34:52 +0100

    Correción en comentarios

--- hypothesis.m
+++ hypothesis.m
@@ -1,4 +1,4 @@
-% Evaluate the hypothesis with all given x
+%% Evaluate the hypothesis with all given x
 function y=hypothesis(x,theta)
   orden=columns(theta)-1;
   x_poly = bsxfun(@power,x,(0:orden));
