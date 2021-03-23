4b708c45f834c30f61602835bba7bce39d4d2ad0
Sofia Acerbi <sofiacerbi00@gmail.com>
2021-03-23 14:05:20 +0100

    Se agrega el apartado 7

--- testdescent.m
+++ testdescent.m
@@ -165,8 +165,8 @@ grid;
 
 
 ######################################################################                         
-##Apartado 4: Evolución de la hipótesis en cada caso para llegar     ##
-##al mínimo.                                                         ##
+## Apartado 4: Evolución de la hipótesis para cada método para      ##
+## llegar al mínimo.                                                ##
 ######################################################################
 
 #Tomado como referencia batch_grad_descent.m (brindado por el profesor)
@@ -177,7 +177,6 @@ areas=linspace(0,maxX,length(Xo));
 nareas=nx.transform(areas);
 
 
-
 ## Método Batch Gradient Descent ## 
 An=hypothesis(nareas',thetas_batch(1,:));
 A=ny.itransform(An);
@@ -185,7 +184,8 @@ A=ny.itransform(An);
 figure(6,"name","Evolución de la hipótesis Batch Gradient Descent");
   
 plot(nx.itransform(X),ny.itransform(Y),"*b",2);
-title({"Evolución de la hipótesis por el método:"," Batch Gradient Descent"},
+title({"Evolución de la hipótesis por el método:",
+              " Batch Gradient Descent"},
               "fontsize", 20);
 axis([0 maxX 0 1000]);
 xlabel('Area',"fontsize", 10);
@@ -204,18 +204,16 @@ endfor;
 
 plot(areas,A,'g',"linewidth",3);
 
-################################################################################
-################################################################################
 
 ## Método Stochastic Gradient Descent ##
-
 An=hypothesis(nareas',thetas_stoch(1,:));
 A=ny.itransform(An);
 
 figure(7,"name","Evolución de la hipótesis Stochastic Gradient Descent");
   
 plot(nx.itransform(X),ny.itransform(Y),"*b",2);
-title({"Evolución de la hipótesis por el método:"," Stochastic Gradient Descent"},
+title({"Evolución de la hipótesis por el método:",
+              " Stochastic Gradient Descent"},
               "fontsize", 20);
 axis([0 maxX 0 1000]);
 xlabel('Area',"fontsize", 10);
@@ -235,8 +233,7 @@ endfor;
 plot(areas,A,'g',"linewidth",3);
 
 
-## Método Stochastic Gradient Descent with momentum ## 
-
+## Método Stochastic Gradient Descent with Momentum ## 
 An=hypothesis(nareas',thetas_mom(1,:));
 A=ny.itransform(An);
 
@@ -263,15 +260,15 @@ endfor;
 plot(areas,A,'g',"linewidth",3);
 
 
-## Método Stochastic Gradient Descent with RMSprop##
-
+## Método Stochastic Gradient Descent with RMSprop ##
 An=hypothesis(nareas',thetas_rms(1,:));
 A=ny.itransform(An);
 
 figure(9,"name","Evolución de la hipótesis Stochastic Gradient Descent");
   
 plot(nx.itransform(X),ny.itransform(Y),"*b",2);
-title({"Evolución de la hipótesis por el método:"," Stochastic Gradient Descent with RMSprop"},
+title({"Evolución de la hipótesis por el método:",
+              " Stochastic Gradient Descent with RMSprop"},
               "fontsize", 20);
 axis([0 maxX 0 1000]);
 xlabel('Area',"fontsize", 10);
@@ -291,15 +288,15 @@ endfor;
 plot(areas,A,'g',"linewidth",3);
 
 
-## Método Stochastic Gradient Descent with ADAM## 
-
+## Método Stochastic Gradient Descent with ADAM ## 
 An=hypothesis(nareas',thetas_adam(1,:));
 A=ny.itransform(An);
 
 figure(10,"name","Evolución de la hipótesis Stochastic Gradient Descent");
   
 plot(nx.itransform(X),ny.itransform(Y),"*b",2);
-title({"Evolución de la hipótesis por el método:"," Stochastic Gradient Descent with ADAM"},
+title({"Evolución de la hipótesis por el método:",
+              " Stochastic Gradient Descent with ADAM"},
               "fontsize", 20);
 axis([0 maxX 0 1000]);
 xlabel('Area',"fontsize", 10);
@@ -319,13 +316,12 @@ endfor;
 plot(areas,A,'g',"linewidth",3);
 
 
-##########################################################################                         
-#### Apartado 5: Evolución del error J(?) en función del número de      ##
-#### iteración, para varios valores de la tasa de aprendizaje.          ##
-##########################################################################
+######################################################################                         
+## Apartado 5: Evolución del error J en función del número de       ##
+## iteración, para varios valores de la tasa de aprendizaje.        ##
+######################################################################
 
-#Método batch gradient descent 
-
+## Método Batch Gradient Descent ##
 t0 = [-1 -0.2 -0.3];
 l_rate=0.01; #tiene que ser menor o igual a 0.01
 maxiter=300;
@@ -338,7 +334,7 @@ method="batch";
                             "maxiter",maxiter);
                            
 
-figure(11,"name","error vs iteracion");
+figure(11,"name","error vs iteración");
 
 
 hold on;
@@ -370,10 +366,11 @@ l_rate=0.0008; #tiene que ser menor o igual a 0.01
                             "maxiter",maxiter);
 iter_batch=[0:1:length(errors_batch)-1]';
 plot(iter_batch,errors_batch,'c','linewidth',2);
+
 legend({'alpha=0.01','alpha=0.005','alpha=0.001','alpha=0.0008'}
                               ,"location","northeastoutside");
-
-title({"Evolución del error J(theta) con distintos ''lr'' por el método:"," Batch Gradient Descent "},
+title({"Evolución del error J(theta) con distintos ''lr'' por el método:",
+              " Batch Gradient Descent "},
               "fontsize", 15);                              
 xlabel('Iterations',"fontsize", 10);
 ylabel('Error',"fontsize", 10); 
@@ -382,13 +379,13 @@ ylim([0 12]);
 grid on;
 hold off;
 
-## Método Stochastic Gradient Descent ##
 
+## Método Stochastic Gradient Descent ##
 t0 = [-1 -0.2 -0.3];
 l_rate=0.009; #debe ser menor a 0.04
 maxiter=500;
 epsilon=0.001;
-minibatch=20; %%0.5*rows(Xo);
+minibatch=20;
 
 [thetas_stoch,errors_stoch]=descentpoly(@loss,@gradloss,t0,X,Y,l_rate,
                             "method","stochastic",
@@ -396,8 +393,8 @@ minibatch=20; %%0.5*rows(Xo);
                             "epsilon",epsilon,
                             "minibatch",minibatch);
                   
-
-figure(12,"name","error vs iteracion");
+                  
+figure(12,"name","error vs iteración");
 hold on;
 iter_stoch=[0:1:length(errors_stoch)-1]';
 plot(iter_stoch,errors_stoch,'b','linewidth',2);
@@ -432,7 +429,6 @@ l_rate=0.0008;
 iter_stoch=[0:1:length(errors_stoch)-1]';
 plot(iter_stoch,errors_stoch,'c','linewidth',2);    
 
-
 legend({'alpha=0.009','alpha=0.005','alpha=0.001','alpha=0.0008'}
                               ,"location","northeastoutside");
 
@@ -445,8 +441,8 @@ ylim([0 40]);
 grid on;
 hold off;
 
-## Método Stochastic Gradient Descent with momentum ## 
 
+## Método Stochastic Gradient Descent with Momentum ## 
 t0 = [-1 -0.2 -0.3];
 l_rate=0.009;
 maxiter=500;
@@ -459,7 +455,7 @@ minibatch=20;
                             "epsilon",epsilon,
                             "minibatch",minibatch);
                          
-figure(13,"name","error vs iteracion");
+figure(13,"name","error vs iteración");
 hold on;
 iter_mom=[0:1:length(errors_mom)-1]';
 plot(iter_mom,errors_mom,'b','linewidth',2);                          
@@ -498,7 +494,6 @@ plot(iter_mom,errors_mom,'c','linewidth',2);
 
 legend({'alpha=0.009','alpha=0.005','alpha=0.001','alpha=0.0008'}
                               ,"location","northeastoutside");
-
 title({"Evolución del error J(theta) con distintos ''lr'' por el método:",
               " Stochastic Gradient Descent with momentum "},
               "fontsize", 15);                              
@@ -509,16 +504,15 @@ ylim([0 40]);
 grid on;
 hold off;
 
+
 ## Método Stochastic Gradient Descent with RMSprop ##
-
-
 t0 = [-1 -0.2 -0.3];
 l_rate=0.05;
 beta=0.9;
 beta2=0.95;
 maxiter=500;
 epsilon=0.005;
-minibatch=20; %%0.5*rows(Xo);
+minibatch=20;
 
 [thetas_rms,errors_rms]=descentpoly(@loss,@gradloss,t0,X,Y,l_rate,
                             "method","rmsprop",
@@ -527,7 +521,8 @@ minibatch=20; %%0.5*rows(Xo);
                             "maxiter",maxiter,
                             "epsilon",epsilon,
                             "minibatch",minibatch);
-figure(14,"name","error vs iteracion");
+                            
+figure(14,"name","error vs iteración");
 hold on;
 iter_rms=[0:1:length(errors_rms)-1]';
 plot(iter_rms,errors_rms,'b','linewidth',2);                            
@@ -570,9 +565,8 @@ l_rate=0.004;
 iter_rms=[0:1:length(errors_rms)-1]';
 plot(iter_rms,errors_rms,'c','linewidth',2);                            
                             
- legend({'alpha=0.05','alpha=0.01','alpha=0.008','alpha=0.004'}
+legend({'alpha=0.05','alpha=0.01','alpha=0.008','alpha=0.004'}
                               ,"location","northeastoutside");
-
 title({"Evolución del error J(theta) con distintos ''lr'' por el método:",
               " Stochastic Gradient Descent with RMSprop "},
               "fontsize", 15);                              
@@ -585,14 +579,12 @@ hold off;
 
 
 ## Método Stochastic Gradient Descent with Adam ##
-
-
 t0 = [-1 -0.2 -0.3];
 l_rate=0.05;
 beta=0.9;
 maxiter=500;
 epsilon=0.005;
-minibatch=20; %%0.5*rows(Xo);
+minibatch=20;
 
 [thetas_adam,errors_adam]=descentpoly(@loss,@gradloss,t0,X,Y,l_rate,
                             "method","adam",
@@ -600,7 +592,8 @@ minibatch=20; %%0.5*rows(Xo);
                             "maxiter",maxiter,
                             "epsilon",epsilon,
                             "minibatch",minibatch);
-figure(15,"name","error vs iteracion");
+                            
+figure(15,"name","error vs iteración");
 hold on;
 iter_adam=[0:1:length(errors_adam)-1]';
 plot(iter_adam,errors_adam,'b','linewidth',2);                            
@@ -638,9 +631,8 @@ l_rate=0.009;
 iter_adam=[0:1:length(errors_adam)-1]';
 plot(iter_adam,errors_adam,'c','linewidth',2);                            
                             
- legend({'alpha=0.05','alpha=0.03','alpha=0.01','alpha=0.009'}
+legend({'alpha=0.05','alpha=0.03','alpha=0.01','alpha=0.009'}
                               ,"location","northeastoutside");
-
 title({"Evolución del error J(theta) con distintos ''lr'' por el método:",
               " Stochastic Gradient Descent with ADAM "},
               "fontsize", 15);                              
@@ -649,6 +641,134 @@ ylabel('Error',"fontsize", 10);
 xlim([0 100]);
 ylim([0 65]);
 grid on;
-hold off;                            
-#### Apartado 7: 
+hold off;    
 
+                        
+########################################################################                         
+## Apartado 7: Comparación de aproximaciones finales para regresiones ##
+## obtenidas con órdenes polinomiales distintas                       ##
+########################################################################
+
+orden = [1 2 3 7]; ##Órdenes que se desean plottear
+
+normalizer_type = "minmax";
+nx = normalizer(normalizer_type);
+X = nx.fit_transform(Xo);  
+ny = normalizer(normalizer_type);
+Y = ny.fit_transform(Yo);
+
+minX = min(Xo);
+maxX = max(Xo);
+
+areas=linspace(0,maxX,length(Xo));
+nareas=nx.transform(areas);
+
+## Método Batch Gradient Descent ##
+precios_poly=polinomial(X,Y,nareas,ny,"batch",orden);
+
+figure(16,"name","Método Batch para distintos órdenes de polinomios");hold off;
+plot(areas,precios_poly(:,1),'b',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,2),'c',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,3),'g',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,4),'r',"linewidth",0.5);hold on;
+
+plot(Xo,Yo,"*b",2);
+legend({cstrcat("n=", mat2str(orden(1))),
+        cstrcat("n=", mat2str(orden(2)))
+        cstrcat("n=", mat2str(orden(3)))
+        cstrcat("n=", mat2str(orden(4)))},"location","northeastoutside");
+title({"Aproximaciones para distintos órdenes de polinomios (n)";
+              "Método Batch Gradient Descent"},
+              "fontsize", 20);
+xlabel('Áreas',"fontsize", 10);
+ylabel('Precios',"fontsize", 10); 
+grid on;
+hold off;
+
+## Método Stochastic Gradient Descent ##
+precios_poly=polinomial(X,Y,nareas,ny,"stochastic",orden);
+
+figure(17,"name","Método Stochastic para distintos órdenes de polinomios");hold off;
+plot(areas,precios_poly(:,1),'b',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,2),'c',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,3),'g',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,4),'r',"linewidth",0.5);hold on;
+
+plot(Xo,Yo,"*b",2);
+legend({cstrcat("n=", mat2str(orden(1))),
+        cstrcat("n=", mat2str(orden(2)))
+        cstrcat("n=", mat2str(orden(3)))
+        cstrcat("n=", mat2str(orden(4)))},"location","northeastoutside");
+title({"Aproximaciones para distintos órdenes de polinomios (n)";
+              "Método Stochastic Gradient Descent"},
+              "fontsize", 20);
+xlabel('Áreas',"fontsize", 10);
+ylabel('Precios',"fontsize", 10); 
+grid on;
+hold off;
+
+## Método Stochastic Gradient Descent with Momentum ##
+precios_poly=polinomial(X,Y,nareas,ny,"momentum",orden);
+
+figure(18,"name","Método Momentum para distintos órdenes de polinomios");hold off;
+plot(areas,precios_poly(:,1),'b',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,2),'c',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,3),'g',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,4),'r',"linewidth",0.5);hold on;
+
+plot(Xo,Yo,"*b",2);
+legend({cstrcat("n=", mat2str(orden(1))),
+        cstrcat("n=", mat2str(orden(2)))
+        cstrcat("n=", mat2str(orden(3)))
+        cstrcat("n=", mat2str(orden(4)))},"location","northeastoutside");
+title({"Aproximaciones para distintos órdenes de polinomios (n)";
+              "Método Stochastic Gradient Descent with Momentum"},
+              "fontsize", 20);
+xlabel('Áreas',"fontsize", 10);
+ylabel('Precios',"fontsize", 10); 
+grid on;
+hold off;
+
+## Método Stochastic Gradient Descent with RMSprop ##
+precios_poly=polinomial(X,Y,nareas,ny,"rmsprop",orden);
+
+figure(19,"name","Método RMSprop para distintos órdenes de polinomios");hold off;
+plot(areas,precios_poly(:,1),'b',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,2),'c',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,3),'g',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,4),'r',"linewidth",0.5);hold on;
+
+plot(Xo,Yo,"*b",2);
+legend({cstrcat("n=", mat2str(orden(1))),
+        cstrcat("n=", mat2str(orden(2)))
+        cstrcat("n=", mat2str(orden(3)))
+        cstrcat("n=", mat2str(orden(4)))},"location","northeastoutside");
+title({"Aproximaciones para distintos órdenes de polinomios (n)";
+              "Método Stochastic Gradient Descent with RMSprop"},
+              "fontsize", 20);
+xlabel('Áreas',"fontsize", 10);
+ylabel('Precios',"fontsize", 10); 
+grid on;
+hold off;
+
+## Método Stochastic Gradient Descent with Adam ##
+precios_poly=polinomial(X,Y,nareas,ny,"adam",orden);
+
+figure(20,"name","Método Adam para distintos órdenes de polinomios");hold off;
+plot(areas,precios_poly(:,1),'b',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,2),'c',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,3),'g',"linewidth",0.5);hold on;
+plot(areas,precios_poly(:,4),'r',"linewidth",0.5);hold on;
+
+plot(Xo,Yo,"*b",2);
+legend({cstrcat("n=", mat2str(orden(1))),
+        cstrcat("n=", mat2str(orden(2)))
+        cstrcat("n=", mat2str(orden(3)))
+        cstrcat("n=", mat2str(orden(4)))},"location","northeastoutside");
+title({"Aproximaciones para distintos órdenes de polinomios (n)";
+              "Método Stochastic Gradient Descent with Adam"},
+              "fontsize", 20);
+xlabel('Áreas',"fontsize", 10);
+ylabel('Precios',"fontsize", 10); 
+grid on;
+hold off;
